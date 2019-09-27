7b1a9fd421419c79ae301bb98705c35ee7724d7e
sergio hoyos <yeyo_hoyos@hotmail.com>
2019-09-27 17:48:45 -0400

    Error

---
+++ evaluation/evaluation/Models/Nota.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace evaluation.Models
+{
+    public enum TypeSubject { Matematica, Biologia, Quimica }
+    public enum TypeState { Aprobado, Reprobado  }
+
+    public class Nota
+    {
+        [Key]
+        public TypeSubject Subject { get; set; }
+        [Required]
+        public int Grades { get; set; }
+        [Required]
+        public TypeState State { get; set; }
+        [Required]
+        public Student Student { get; set; }
+
+
+    }
+}
---
+++ evaluation/evaluation/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace evaluation.Models
+{
+    public enum TypeGender{ Male,Female,Other }
+
+
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public  TypeGender Gender { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public int Date { get; set; }
+        [Required]
+        public virtual ICollection<Nota> Notas { get; set; }
+
+    }
+}
